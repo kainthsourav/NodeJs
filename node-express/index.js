28efebae109c52c02fb430c7b40e261d61fbed9a
kainthsourav <souravkainth@outlook.com>
2020-04-29 13:50:42 +0530

    Morgan

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const http=require('http');
+const morgan =require('morgan');
 
 const hostname='localhost';
 const port=3000;
 
 const app=express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname+ '/public'));
 
 app.use((req,res,next)=>
 {
@@ -13,7 +17,6 @@ app.use((req,res,next)=>
   res.setHeader('Content-Type','text-html');
   res.end('<html><body><h1>Express Server</h1></body></html>');
 });
-
 
 const server=http.createServer(app);
 
