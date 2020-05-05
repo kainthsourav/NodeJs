11c2def7db3464b5270ec32202438f4304d92a12
kainthsourav <souravkainth@outlook.com>
2020-05-05 12:59:12 +0530

    Callback Hell and Promises

--- node_mongo/index.js
+++ node_mongo/index.js
@@ -7,56 +7,39 @@ const dboper=require('./operations');
 const url='mongodb://localhost:27017/';
 const dbname='confusion';
 
-MongoClient.connect(url,(err,client)=>{
-    
-    assert.equal(err,null);
 
-    console.log('Connected to Server');
-
+MongoClient.connect(url).then((client)=>
+{
+    console.log("Connected to Server ");
     const db=client.db(dbname);
-    // const collection=db.collection("dishes");
-
-    // collection.insertOne({"name":"Test","description":"test"},(err,result)=>
-    // {
-    //     assert.equal(err,null);
-
-    //     console.log('Inserted :\n');
-    //     console.log(result.ops);
-    //     collection.find({}).toArray((err,docs)=>{
-    //        assert.equal(err,null);
-    //        console.log("Found : \n");
-    //        console.log(docs);
-    //        db.dropCollection("dishes",(err,result)=>
-    //        {
-    //            assert.equal(err,null);
-
-    //            client.close();
-    //        });
-    //     });
-    // });
-    dboper.insertDocument(db,{name:"Sourav",description:"Kainth"},"dishes",(result)=>
+    
+    dboper.insertDocument(db,{name:"Sourav",description:"Test"},"dishes")
+    .then((result)=>
     {
-      console.log('Insert Document: \n',result.ops);
-     
-      dboper.findDocument(db,"dishes",(docs)=>
-      {
-         console.log('Found Document: \n',docs);
-        
-         dboper.updateDocument(db,{name:"Sourav"},{description:"Updated Test"},"dishes",(result)=>
-         {
-             console.log('Updated Document :\n',result.result);
-
-             dboper.findDocument(db,"dishes",(docs)=>
-             {
-                console.log('Found the Updated Document :\n',result.result);
-                db.dropCollection("dishes",(result)=>
-                {
-                    console.log("Dropped Collection",result);
-                    client.close();
-                });
-             });
-
-         });
-      });
-    });
+        console.log("Insert document \n "+result.ops);
+        return dboper.findDocuments(db,"dishes");
+    })
+    .then((docs)=>
+    {
+        console.log("Found Docuements: \n",docs);
+        CollectionName:string="dishes";
+        return dboper.updateDocument(db,{name: "Sourav Kainth",description:"Updated"},CollectionName);
+    })
+    .then((result)=>
+    {
+        console.log("Document Updated "+result.result);
+        return dboper.findDocuments(db,"dishes");
+    })
+    .then((docs)=>
+    {
+        console.log("Found the Updated Document:\n",docs);
+        return db.dropCollection("dishes");
+    })
+    .then((result)=>
+    {
+        console.log("Dropped Collection ",result);
+        return client.close();
+    })
+    .catch((err)=>console.log(err));
 })
+.catch((err)=>console.log(err));
