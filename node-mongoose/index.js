cf94c31160bed67ed86f2e4c1bfdeabbd42fd253
kainthsourav <souravkainth@outlook.com>
2020-05-05 15:05:20 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,25 +8,42 @@ connect.then((db)=>
 {
     console.log("Connected to Server");
 
-    var newDish=Dishes({
+    Dishes.create({
         name:'Uthappizza',
-        description:'test'
-    });
-    
-    newDish.save().then((dish)=>
-    {
-        console.log(dish);
-        return Dishes.find({});
+        description:'Test'
     })
-    .then((dishes)=>
-    {
+    .then((dish)=>{
+        console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)=>{
         console.log(dishes);
         return Dishes.remove({});
     })
-    .then(()=>
-    {
+    .then(()=>{
         return mongoose.connection.close();
     })
     .catch((err)=>console.log(err));
+
+    // var newDish=Dishes({
+    //     name:'Uthappizza',
+    //     description:'test'
+    // });
+    
+    // newDish.save().then((dish)=>
+    // {
+    //     console.log(dish);
+    //     return Dishes.find({});
+    // })
+    // .then((dishes)=>
+    // {
+    //     console.log(dishes);
+    //     return Dishes.remove({});
+    // })
+    // .then(()=>
+    // {
+    //     return mongoose.connection.close();
+    // })
+    // .catch((err)=>console.log(err));
 
 });
